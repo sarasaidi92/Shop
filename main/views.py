e62a1f3a58b99208c62de28b18e73f971c4c01f9
sarasaidi92 <160146978+sarasaidi92@users.noreply.github.com>
2025-04-12 13:11:21 +0330

    create index.html,about.html,master.html

--- main/views.py
+++ main/views.py
@@ -2,5 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse("Shop")
+    return render(request, template_name='index.html')
 
+def about(request):
+    return render(request, template_name='about.html')
