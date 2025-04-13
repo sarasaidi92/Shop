5d137c34ced696ab7b5ed7425867c0bea01cf60b
sarasaidi92 <160146978+sarasaidi92@users.noreply.github.com>
2025-04-13 11:06:42 +0330

    create single_detail

--- products/views.py
+++ products/views.py
@@ -1,3 +1,4 @@
 from django.shortcuts import render
 
-# Create your views here.
+def products(request):
+    return render(request, 'products.html')
--- shop/urls.py
+++ shop/urls.py
@@ -4,4 +4,5 @@ from django.urls import path, include
 urlpatterns = [
     path('sara_log/', admin.site.urls),
     path('', include('main.urls')),
+    path('products/', include('products.urls')),
 ]
