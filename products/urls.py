f614af666c8c526878415e4a2c9e27697934c812
sarasaidi92 <160146978+sarasaidi92@users.noreply.github.com>
2025-04-19 11:07:05 +0330

    add paginator

--- products/urls.py
+++ products/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 from .views import products, single_product
+
+
 urlpatterns = [
     path('', products, name='products'),
-    path('single', single_product, name='single_product'),
-
+    path('single/', single_product, name='single_product'),
 
 ]
--- products/views.py
+++ products/views.py
@@ -1,7 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Product
+from django.core.paginator import Paginator
 
 def products(request):
-    return render(request, 'products.html')
+    listings = Product.objects.order_by('-product_date')
+    paginator = Paginator(listings, 1)
+    page = request.GET.get('page')
+    paged_products = paginator.get_page(page)
+
+    context = {
+        'products': paged_products,
+    }
+    return render(request, 'products.html', context=context)
 
 def single_product(request):
+
     return render(request, 'single_product.html')
